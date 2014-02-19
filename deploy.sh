d20c725b8f7e4b7c81bff074fbd38d4197623068
Matthias Luedtke <matthias.luedtke@dynport.de>
2014-02-19 19:28:55 +0100

    Set both heroku configs at once.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@
 set -e
 
 git push heroku master
-heroku config:set GIT_REVISION=`git describe --always`
-heroku config:set DEPLOYED_AT=`date +%s`
+heroku config:set GIT_REVISION=`git describe --always` DEPLOYED_AT=`date +%s`
 
